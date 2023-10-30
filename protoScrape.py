e4de5189e315ad94a8044fb9c16ea260c60b1a81
wofow <133924560+wofow@users.noreply.github.com>
2023-10-30 15:37:10 -0700

    try block
    update error handling

--- protoScrape.py
+++ protoScrape.py
@@ -2,43 +2,74 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
+import logging
 
 
-def extract_html_and_css(url):
-    # Send an HTTP GET request to the URL
-    response = requests.get(url)
+def extract_html_and_css_recursive(url, visited_links):
+    html_content, css_content, page_links = None, None, []
+    try:
+        if url in visited_links:
+            # Skip URLs that have already been visited to avoid infinite loops
+            return html_content, css_content, page_links
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the HTML content of the page
-        soup = BeautifulSoup(response.text, 'html.parser')
+        visited_links.add(url)
 
-        # Extract HTML content
-        html_content = str(soup)
+        # Send an HTTP GET request to the URL
+        response = requests.get(url)
 
-        # Extract CSS styles
-        css_links = []
-        css_content = ""
-        for link in soup.find_all('link', rel='stylesheet'):
-            css_url = link.get('href')
-            # Create an absolute URL by joining with the base URL
-            css_url = urljoin(url, css_url)
-            css_response = requests.get(css_url)
-            if css_response.status_code == 200:
-                css_links.append(css_url)
-                css_content += css_response.text
+        # Check the response status code
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-        return html_content, css_content, css_links
-    else:
-        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
-        return None, None, None
+            # Extract HTML content
+            html_content = str(soup)
+
+            # Extract CSS styles
+            css_links = []
+            css_content = ""
+            for link in soup.find_all('link', rel='stylesheet'):
+                css_url = link.get('href')
+                css_url = urljoin(url, css_url)
+                css_response = requests.get(css_url)
+
+                if css_response.status_code == 200:
+                    css_links.append(css_url)
+                    css_content += css_response.text
+
+            # Extract links from the HTML content
+            page_links = extract_links(html_content, url)
+            for new_link in page_links:
+                extracted_html, extracted_css, _ = extract_html_and_css_recursive(new_link, visited_links)
+                html_content += extracted_html
+                css_content += extracted_css
+
+        return html_content, css_content, page_links
+
+    except requests.exceptions.RequestException as e:
+
+        error_message = f"Request failed for URL {url}: {str(e)}"
+        logging.error(error_message)
+
+        if isinstance(e, requests.exceptions.Timeout):
+            logging.error("Timeout error.")
+
+        elif isinstance(e, requests.exceptions.ConnectionError):
+            logging.error("Connection error.")
+
+        # Handle other error types as needed
+
+        return html_content, css_content, page_links
+    except Exception as ex:
+        # Handle other unexpected exceptions
+        logging.error(f"An unexpected error occurred for URL {url}: {str(ex)}")
+        return html_content, css_content, page_links
 
 
 def save_to_files(content, filename, extension):
     if content:
         with open(f'{filename}.{extension}', 'w', encoding='utf-8') as file:
             file.write(content)
-        print(f"{extension.upper()} content saved to '{filename}.{extension}'.")
+        logging.info(f"{extension.upper()} content saved to '{filename}.{extension}'.")
 
 
 def extract_links(html_content, base_url):
@@ -51,75 +82,55 @@ def extract_links(html_content, base_url):
     return links
 
 
-def run(url, base_filename):
-    # Create a list to store visited links
-    visited_links = set()
+def extract_domain(url):
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+    return domain
 
-    # List to store all links
+
+def run(url, base_filename):
+    visited_links = set()
     all_links = [url]
 
     while all_links:
         link = all_links.pop(0)
+        domain = extract_domain(link)
+
+        # Use the domain to create a directory structure
+        directory_structure = os.path.join(base_filename, domain)
+        filename = os.path.join(directory_structure, "index")
+
+        # Create directories if they don't exist
+        os.makedirs(directory_structure, exist_ok=True)
 
         # Send an HTTP GET request to the URL
         response = requests.get(link)
 
-        if response.status_code == 400:
-            print(print(f"Failed to retrieve the webpage. Status code: 400 ({link})"))
-            continue  # Skip to the next line
+        # Check the response status code
+        if response.status_code == 200:
+            # Parse the HTML content of the page
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-        if response.status_code == 401:
-            print(print(f"Failed to retrieve the webpage. Status code: 401 ({link})"))
-            continue  # Skip to the next line
+            # Extract HTML content
+            html_content = str(soup)
 
-        if response.status_code == 403:
-            print(print(f"Failed to retrieve the webpage. Status code: 403 ({link})"))
-            continue  # Skip to the next link
-
-        if response.status_code == 404:
-            print(print(f"Failed to retrieve the webpage. Status code: 404 ({link})"))
-            continue  # Skip to the next link
-
-        if response.status_code == 500:
-            print(print(f"Failed to retrieve the webpage. Status code: 500 ({link})"))
-            continue  # Skip to the next line
-
-        if response.status_code == 504:
-            print(print(f"Gateway Timeout. Status code: 504 ({link})"))
-            continue  # Skip to the next line
-
-        if response.status_code == 429:
-            print(print(f"Too many requests. Status code: 429 ({link})"))
-            continue
-
-        parsed_url = urlparse(link)
-        domain_path = parsed_url.netloc + parsed_url.path
-        filename = os.path.join(base_filename, domain_path)
-
-        # Create directories if they don't exist
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-
-        html_content, css_content, page_links = extract_html_and_css(link)
-
-        # If html_content is None, it means the page is not available (404)
-        if html_content is not None:
-            # Save the page
+            # Process and save HTML and CSS content
             save_to_files(html_content, filename, 'html')
-            save_to_files(css_content, filename, 'css')
 
-        visited_links.add(link)  # Mark the current link as visited
+            # Extract links from the HTML content
+            page_links = extract_links(html_content, link)
 
-        # Extract links from the HTML content
-        page_links = extract_links(html_content, link)
+            # Add new links to the list for further processing
+            for new_link in page_links:
+                if new_link not in visited_links:
+                    all_links.append(new_link)
 
-        # Add new links to the list
-        for new_link in page_links:
-            if new_link not in visited_links:
-                all_links.append(new_link)
+        visited_links.add(link)
 
 
 if __name__ == '__main__':
-    main_base_url = input("Enter the base URL: ")
-    main_base_filename = input("Enter a base filename (without extension): ")
+    # Initialize logging
+    logging.basicConfig(filename='web_scraping.log', level=logging.INFO)
 
-    run(main_base_url, main_base_filename)
+    main_base_url = input("Enter the base URL: ")
+    run(main_base_url, main_base_url)
